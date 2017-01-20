b303e044e28cc9bbd66fad1aa0f1170e4fbbb3b3
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2017-01-19 19:20:49 -1000

    fixed issue

--- todo_app.sql
+++ todo_app.sql
@@ -22,11 +22,14 @@ ALTER TABLE "tasks" ADD COLUMN completed_at timestamp DEFAULT NULL;
 ALTER TABLE "tasks" ALTER COLUMN updated_at SET DEFAULT now();
 ALTER TABLE "tasks" ALTER COLUMN updated_at SET NOT NULL;
 
-INSERT INTO "tasks" (title, description, created_at, updated_at, completed_at)
-VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
+INSERT INTO "tasks"
+VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
 
-INSERT INTO "tasks" (title, description)
-VALUES ('Study PostgreSQL', 'Read all the documentation');
+INSERT INTO "tasks"
+VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation', DEFAULT , DEFAULT , DEFAULT);
+
+SELECT *
+FROM "tasks";
 
 SELECT title
 FROM "tasks"
